9cecf546d9050d894f750bd829347799e7802859
Mary Karakida <makarakida@gmail.com>
2022-08-31 18:36:35 -0300

    initial commit

---
+++ src/middlewares/errorMiddleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+
+function errorHandler(err, _req: Request, res: Response, _next: NextFunction) {
+    if (err.type === 'error_unauthorized') {
+        return res.status(401).send(err.message);
+    }
+
+    if (err.type === 'error_forbidden') {
+        return res.status(403).send(err.message);
+    }
+
+    if (err.type === 'error_not_found') {
+        return res.status(404).send(err.message);
+    }
+
+    if (err.type === 'error_conflict') {
+        return res.status(409).send(err.message);
+    }
+
+    if (err.type === 'error_unprocessable_entity') {
+        return res.status(422).send(err.message);
+    }
+
+    return res.status(500).send(err.message);
+}
+
+export default errorHandler;
